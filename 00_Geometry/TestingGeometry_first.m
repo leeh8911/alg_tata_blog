15df86b07aa52c63899d88c3de189ce1646bbd28
sangwon <leeh8911@gamil.com>
2019-12-23 22:43:29 +0900

    <Update Point2D>

--- 00_Geometry/TestingGeometry_first.m
+++ 00_Geometry/TestingGeometry_first.m
@@ -4,6 +4,7 @@ f = figure;
 ax = gca;
 hold on; grid on; box on;
 axis([-10 10 -10 10]);
+xlabel("\it{x}"); ylabel("\it{y}");
 %% step1. initialize point2d 
 
 pntA = Point2D(3,4);    % there is initial values
@@ -14,7 +15,18 @@ pntB = Point2D();       % there is no initial value(pntB = [0, 0])
 cla;
 pntA.Draw(ax, 'r','x');
 pntB.Draw(ax, 'g','o');
+title("create and draw point2d");
 saveas(gcf, "./fig/fig1 - draw pnt2d.jpg")
+
+%% step3. calculate distance and print distance
+
+cla;
+pntA.Draw(ax, 'r','x');
+pntB.Draw(ax, 'g','o');
+pntA.DistToTarget(pntB);
+pntA.PrintDist(pntB, ax, 'r','x');
+title("add method and print distance");
+saveas(gcf, "./fig/fig1 - draw pnt2d_function.jpg")
 
 
 %% step3. initialize line2d
